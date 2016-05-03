3d91359c85e79fb2fd0c936c2bb9a4c64324f83d
zetton110 <locolv.kohe@gmail.com>
2016-05-04 03:19:44 +0900

    再度チャレンジ

--- blog/js/app.js
+++ blog/js/app.js
@@ -6,11 +6,24 @@ app.config(function($routeProvider){
     
     .when('/',{
         templateUrl: 'pages/home.html',
+        controller: 'homeController'
     })
     .when('/about',{
         templateUrl: 'pages/about.html',
+        controller: 'aboutController'
     })
     .when('/project',{
         templateUrl: 'pages/project.html',
+        controller: 'projectController'
     })    
 });
+
+app.controller('homeController',['$scope',function($scope){
+    
+}]);
+app.controller('aboutController',['$scope',function($scope){
+    
+}]);
+app.controller('projectController',['$scope',function($scope){
+    
+}]);
